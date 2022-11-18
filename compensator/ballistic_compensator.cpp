652541dde6e5aebec400175b99f0904c18b8e5b0
catslashbin <catslashbin@gmail.com>
2022-11-18 19:45:51 +0800

    Ballistic Compensator: Use analytical derivative

--- compensator/ballistic_compensator.cpp
+++ compensator/ballistic_compensator.cpp
@@ -8,16 +8,31 @@
 
 std::function<float(float)> BallisticCompensator::get_ballistic_func(float v0, float x, float y)
 {
-    return [v0, x, y, this](float theta){return ballistic(v0, x, theta) - y;};
+    return [v0, x, y, this](float theta) { return ballistic(v0, x, theta) - y; };
+}
+
+std::function<float(float)> BallisticCompensator::get_ballistic_der_func(float v0, float x)
+{
+    return [v0, x, this](float theta) { return ballistic_der(v0, x, theta); };
+}
+
+float BallisticCompensator::ballistic_der(float v0, float x, float theta) const
+{
+    float Sec = 1 / cosf(theta);
+    float Tan = tanf(theta);
+    float ret = -(G * x * Tan * Sec) / (k * v0 * (1 - (k * x * Sec) / v0)) +
+                x * (Sec * Sec + (G * Sec * Tan) / (k * v0));
+    return ret;
 }
 
 float BallisticCompensator::ballistic(float v0, float x, float theta) const
 {
     return G / (k * k) * logf(1 - (k * x) / (v0 * cosf(theta))) +
-            x * (G / (k * v0 * cosf(theta)) + tanf(theta));
+           x * (G / (k * v0 * cosf(theta)) + tanf(theta));
 }
 
 float BallisticCompensator::calc_shoot_angle(float v0, float x, float y)
 {
-    return EquationSolver::solve(get_ballistic_func(v0, x, y));
+    return EquationSolver::solve_with_der(get_ballistic_func(v0, x, y),
+                                          get_ballistic_der_func(v0, x));
 }
--- compensator/ballistic_compensator.h
+++ compensator/ballistic_compensator.h
@@ -5,7 +5,7 @@
 #ifndef CYGNOIDES_DECISION_BALLISTIC_COMPENSATOR_H
 #define CYGNOIDES_DECISION_BALLISTIC_COMPENSATOR_H
 
-#define G 9.780
+#define G 9.780f
 
 #include "functional"
 
@@ -15,12 +15,35 @@ class BallisticCompensator
 private:
     std::function<float(float)> get_ballistic_func(float v0, float x, float y);
 
+    std::function<float(float)> get_ballistic_der_func(float v0, float x);
+
+    float ballistic_der(float v0, float x, float theta) const;
+
 public:
+
+    /**
+     * Ballistic compensate calculator.
+     * @param k Wind resistance coefficient
+     */
     explicit BallisticCompensator(float k)
     { this->k = k; };
 
+    /**
+     * Calculate ballistic.
+     * @param v0 Initial velocity of the bullet
+     * @param x Horizontal distance to barrel
+     * @param theta Pitch of barrel, in rad
+     * @return Vertical distance to barrel
+     */
     float ballistic(float v0, float x, float theta) const;
 
+    /**
+     * Find shoot angle to the target.
+     * @param v0 Initial velocity of the bullet
+     * @param x Horizontal distance to the target
+     * @param y Vertical distance to the target
+     * @return Pitch of barrel, in rad
+     */
     float calc_shoot_angle(float v0, float x, float y);
 };
 
