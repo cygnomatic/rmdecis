a6bf1dd73751e4871ff22be46554a7dcbbab04c2
catslashbin <catslashbin@gmail.com>
2023-01-05 19:45:37 +0800

    refactor: move types about armor into armor.h

--- typing/general.h
+++ typing/general.h
@@ -18,6 +18,8 @@
 #include <spdlog/fmt/ostr.h>
 #include <spdlog/sinks/stdout_sinks.h>
 
+#include "armor.h"
+
 using namespace cv;
 using namespace spdlog;
 
@@ -42,147 +44,19 @@ struct Time
     }
 };
 
-
 /**
- * Struct for the four corners of an Armor.
- * Corners coord in 2D.
+ * The output of the Vision Part.
+ * @param time The time **the frame is shot**. NOT THE TIME FINISH THE VISION PROCESS.
+ * @param armor_type Type of armor.
+ * @param corners_img_coord Detected armor corners in image coord.
+ * @param confidence Confidence of the prediction.
  */
-struct ArmorCorners2d
+struct ArmorPredResult
 {
-    ArmorCorners2d() = default;
-
-    /**
-     * Construct ArmorCorners2d with four corners coord.
-     * @param corners Corners. Start form Top-Right, goes counter-clockwise to Down-Right.
-     */
-    explicit ArmorCorners2d(cv::Point2f corners[])
-    {
-        tr = corners[0];
-        tl = corners[1];
-        dl = corners[2];
-        dr = corners[3];
-    }
-
-    /**
-     * Construct ArmorCorners2d with four corners coord.
-     * @param corners Corners. Start form Top-Right, goes counter-clockwise to Down-Right.
-     */
-    explicit ArmorCorners2d(std::vector<cv::Point2f> corners)
-    {
-        assert(corners.size() == 4);
-        tr = corners[0];
-        tl = corners[1];
-        dl = corners[2];
-        dr = corners[3];
-    }
-
-    cv::Point2f tr; // Top-Right
-    cv::Point2f tl; // Top-Left
-    cv::Point2f dl; // Down-Left
-    cv::Point2f dr; // Down-Right
-
-    cv::Point2f &operator[](int index)
-    {
-        switch (index)
-        {
-            case 0:
-                return tr;
-            case 1:
-                return tl;
-            case 2:
-                return dl;
-            case 3:
-                return dr;
-            default:
-                throw std::range_error("Corner index should be in 0 to 3.");
-        }
-    }
-
-    explicit operator std::vector<cv::Point2f>() const
-    {
-        return std::vector<cv::Point2f>({tr, tl, dl, dr});
-    }
-
-    /**
-     * Get the bounding box of Armor corners.
-     * @return The bounding rect.
-     */
-    explicit operator Rect2f() const
-    {
-        return boundingRect(std::vector<cv::Point2f>({tr, tl, dl, dr}));
-//        return {Point2f(min(tl.x, dl.x), min(dr.y, dl.y)), Point2f(max(tr.x, tl.x), max(tr.y, tl.y))};
-    }
-
-    //    cv::Point2f center() const
-    //    {
-    //        return (tr + tl + dl + dr) / 4;
-    //    }
-    //
-    //    float area() const
-    //    {
-    //        return (float) (norm((tr - tl).cross(tr - dl)) + norm((dr - dl).cross(dr - tl)));
-    //    }
-    //
-    //    float ratio() const
-    //    {
-    //        return (max(tr.y, tl.y) - min(dr.y, dl.y)) / (max(tr.x, tl.x) - min(tl.x, dl.x));
-    //    }
-
-};
-
-struct ArmorCorners3d
-{
-    ArmorCorners3d() = default;
-
-    explicit ArmorCorners3d(cv::Point3f corners[])
-    {
-        tr = corners[0];
-        tl = corners[1];
-        dl = corners[2];
-        dr = corners[3];
-    }
-
-    explicit ArmorCorners3d(std::vector<cv::Point3f> corners)
-    {
-        assert(corners.size() == 4);
-        tr = corners[0];
-        tl = corners[1];
-        dl = corners[2];
-        dr = corners[3];
-    }
-
-    cv::Point3f tr; // Top-Right
-    cv::Point3f tl; // Top-Left
-    cv::Point3f dl; // Down-Left
-    cv::Point3f dr; // Down-Right
-
-    cv::Point3f &operator[](int index)
-    {
-        switch (index)
-        {
-            case 0:
-                return tr;
-            case 1:
-                return tl;
-            case 2:
-                return dl;
-            case 3:
-                return dr;
-            default:
-                throw std::range_error("Corner index should be in 0 to 3.");
-        }
-    }
-
-    explicit operator std::vector<cv::Point3f>() const
-    {
-        return std::vector<cv::Point3f>({tr, tl, dl, dr});
-    }
-
-    cv::Point3f getCenter() const
-    {
-        return (tr + tl + dl + dr) / 4;
-    }
-
+    Time time{};
+    ArmorID armor_type = UNKNOWN;
+    ArmorCorners2d corners_img_coord;
+    float confidence{};
 };
 
 struct AxisAngles3f
@@ -196,7 +70,6 @@ struct AxisAngles3f
         yaw = angles[2];
     }
 };
-
 
 struct AxisAngles2f
 {
@@ -226,55 +99,21 @@ struct Transform3f
 
     Transform3f(cv::Mat rvec, cv::Mat tvec)
     {
-        this->rvec = rvec;
-        this->tvec = tvec;
+        this->rvec = std::move(rvec);
+        this->tvec = std::move(tvec);
     }
 
-    std::vector<cv::Point3f> apply(const std::vector<cv::Point3f> &pts) const
-    {
-        cv::Mat inp(pts);
-        std::vector<cv::Point3f> ret;
-
-        cv::Mat tmp = rvec * inp + tvec;
-
-        tmp.copyTo(ret);
-        return ret;
-    }
+    // std::vector<cv::Point3f> apply(const std::vector<cv::Point3f> &pts) const
+    // {
+    //     cv::Mat inp(pts);
+    //     std::vector<cv::Point3f> ret;
+    //
+    //     cv::Mat tmp = rvec * inp + tvec;
+    //
+    //     tmp.copyTo(ret);
+    //     return ret;
+    // }
 };
 
-static const int NUM_ARMOR_ID = 11;
-
-
-enum ArmorID
-{
-    UNKNOWN = 0,
-
-    HERO_1 = 1,
-
-    ENGINEER_2 = 2,
-    STANDARD_3 = 3,
-
-    STANDARD_4 = 4,
-    STANDARD_5 = 5,
-    SENTRY_7 = 7,
-
-    BASE_8 = 8,
-    OUTPOST_10 = 10,
-};
-
-/**
- * The output of the Vision Part.
- * @param time The time **the frame is shot**. NOT THE TIME FINISH THE VISION PROCESS.
- * @param armor_type Type of armor.
- * @param corners_img_coord Detected armor corners in image coord.
- * @param confidence Confidence of the prediction.
- */
-struct ArmorPredResult
-{
-    Time time;
-    ArmorID armor_type;
-    ArmorCorners2d corners_img_coord;
-    float confidence;
-};
 
 #endif //CYGNOIDES_TYPING_GENERAL_H
