f70634436127e4aec35623f14d555c1912b5e46d
catslashbin <catslashbin@gmail.com>
2023-01-06 15:47:17 +0800

    test: Update test_reconstructor.cpp

--- tests/test_reconstructor.cpp
+++ tests/test_reconstructor.cpp
@@ -7,6 +7,8 @@
 #include "../src/reconstructor/camera_calib.h"
 #include "../src/utils/cv_utils.h"
 
+#include <fmt/format.h>
+
 int main()
 {
     SimulateVisionOutput vision_output("../../data/vision_out/vision_result.yaml");
@@ -27,14 +29,19 @@ int main()
             drawArmorCorners(frame, pred_result.corners_img_coord, {0, 0, 255});
             auto trans_model2cam = camera_calib.armorSolvePnP(ArmorCorners3d(SMALL_ARMOR), pred_result.corners_img_coord);
 
-            String dist = "Distance: " + std::to_string(norm(trans_model2cam.tvec) / 1000);
-            putText(frame, dist, {50, 200}, FONT_HERSHEY_SIMPLEX, 2, {255, 255, 255}, 3);
+            // String dist = "Distance: " + std::to_string(norm(trans_model2cam.tvec) / 1000);
+            // putText(frame, dist, {50, 200}, FONT_HERSHEY_SIMPLEX, 2, {255, 255, 255}, 3);
+
+            String rot = fmt::format("Rvec [x: {:.2f}, y: {:.2f}, z: {:.2f}]", trans_model2cam.rvec.at<double>(0), trans_model2cam.rvec.at<double>(1), trans_model2cam.rvec.at<double>(2));
+            putText(frame, rot, {50, 200}, FONT_HERSHEY_SIMPLEX, 2, {255, 255, 255}, 3);
+
+            // info("Rvec [x: {:.2f}, y: {:.2f}, z: {:.2f}]", trans_model2cam.rvec.at<double>(0), trans_model2cam.rvec.at<double>(1), trans_model2cam.rvec.at<double>(2));
 
             auto reprojected_corners =  (ArmorCorners2d) camera_calib.projectToImage(std::vector<Point3f>(ArmorCorners3d(SMALL_ARMOR)), trans_model2cam);
             drawArmorCorners(frame, reprojected_corners, {255, 255, 0});
 
             camera_calib.drawAxes(frame, trans_model2cam);
-            // break;
+            break;
         }
 
         player.update(frame);
