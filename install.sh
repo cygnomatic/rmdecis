54f9adbdcbf677f53291cbc12ce7021b3268d0a8
catslashbin <catslashbin@gmail.com>
2023-02-09 17:33:49 +0800

    feat: Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo ">>> Start installing rmdecis lib ..."
 mkdir -p build
 echo ">>> Installing dependency with Conan ..."
-conan install . --install-folder=./build --build=missing -s build_type=Release -c tools.system.package_manager:mode=install
+sudo conan install . --install-folder=./build --build=missing -s build_type=Release -c tools.system.package_manager:mode=install
 echo ">>> Building rmdecis lib ..."
 cmake -DCMAKE_BUILD_TYPE=Release -S . -B ./build
 cmake --build ./build --target rmdecis --parallel $(($(nproc) - 1))
