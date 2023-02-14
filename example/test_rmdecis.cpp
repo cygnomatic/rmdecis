e6e8bf6f56aabec948be01ca5a1f398eb415ff47
catslashbin <catslashbin@gmail.com>
2023-02-14 20:13:39 +0800

    chore: Update test_rmdecis.cpp

--- example/test_rmdecis.cpp
+++ example/test_rmdecis.cpp
@@ -7,7 +7,7 @@
 
 using namespace cv;
 
-DetectArmorsFrame getDetectionResult() {
+FrameInput getDetectionResult() {
     // Construct DetectArmorInfo
     auto corners = ArmorCorners2d({100, 200}, {100, 300}, {200, 200}, {200, 300});
     auto armor = DetectArmorInfo(STANDARD_3, corners, 1.0);
@@ -17,7 +17,7 @@ DetectArmorsFrame getDetectionResult() {
     Time time = {34567654, 122};
     std::vector<DetectArmorInfo> armor_info = {armor};
 
-    return DetectArmorsFrame{seq_idx, time, armor_info};
+    return FrameInput(seq_idx, time, armor_info, RobotState{0, 0, 15});
 }
 
 int main() {
