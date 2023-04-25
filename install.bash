aba1410f14e9197a278b67f3e7dfaf79587e64a6
catslashbin <catslashbin@gmail.com>
2023-04-25 08:19:08 +0800

    chore: Using Ubuntu version to determine whether RMDECIS_DEPLOYMENT should be enabled

--- install.bash
+++ install.bash
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-ARCH=$(uname -m)
-
 echo ">>> Start installing rmdecis lib ..."
 mkdir -p build
 
-if [ "${ARCH}" == "x86_64" ]; then
-  echo ">>> Detected arch: amd64. Disable RMDECIS_DEPLOYMENT"
-  cmake -DCMAKE_BUILD_TYPE=Release -S . -B ./build -G Ninja
-elif [ "${ARCH}" == "aarch64" ]; then
-  echo ">>> Detected arch: aarch64. Enable RMDECIS_DEPLOYMENT."
+UBUNTU_VER=$(lsb_release -r | awk '{print $2}')
+if [ "$UBUNTU_VER" == "20.04" ]; then
+  echo ">>> Detected Ubuntu 20.04. Enable RMDECIS_DEPLOYMENT."
   cmake -DCMAKE_BUILD_TYPE=Release -DRMDECIS_DEPLOYMENT=ON -S . -B ./build -G Ninja
-else
-  echo "(${ARCH}) not supported. Fallback to amd64."
+elif [ "$UBUNTU_VER" == "22.04" ]; then
+  echo ">>> Detected Ubuntu 22.04. Disable RMDECIS_DEPLOYMENT"
   cmake -DCMAKE_BUILD_TYPE=Release -S . -B ./build -G Ninja
+else
+  echo "Unsupported distribution. Please install manually."
+  exit 0
 fi
 
 echo ">>> Building rmdecis lib ..."
