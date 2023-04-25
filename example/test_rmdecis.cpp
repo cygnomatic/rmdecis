f5a83c8717ce0e0fffa42a17f9f77a4709262a01
catslashbin <catslashbin@gmail.com>
2023-04-25 08:43:34 +0800

    chore: Update examples

--- example/test_rmdecis.cpp
+++ example/test_rmdecis.cpp
@@ -8,26 +8,29 @@
 using namespace cv;
 
 ArmorFrameInput getDetectionResult() {
-    // Construct DetectArmorInfo
+    /* Construct DetectArmorInfo */
     auto corners = RectCorners2f({100, 200}, {100, 300}, {200, 200}, {200, 300});
     auto armor = DetectArmorInfo(STANDARD_3, corners, 1.0);
 
-    // Construct DetectArmorsFrame
-    int seq_idx = 0;
-    Time time = {34567654, 122};
-    std::vector<DetectArmorInfo> armor_info = {armor};
+    /* Construct DetectArmorsFrame */
+    int seq_idx = 0; // Frame seq index
+    Time time = {43453, 122}; // Frame capture time
+    std::vector<DetectArmorInfo> armor_info = {armor}; // Detected armor list
 
-    return ArmorFrameInput(seq_idx, time, RobotState{0, 0, 15}, armor_info);
+    /* Construct RobotState */
+    auto robot_state = RobotState(0.f, 0.f, 15000, true);
+
+    return {seq_idx, time, robot_state, armor_info};
 }
 
 int main() {
-    // Load config
+    /* Load config */
     Config cfg("../config/config.yml");
     BasicAiming basic_aiming(cfg);
 
-    // Predict
+    /* Predict */
     EulerAngles angles = basic_aiming.update(getDetectionResult());
 
-    // Print result (should be "0 0")
+    /* Print result (should be "nan nan") */
     std::cout << angles.pitch << " " << angles.yaw << std::endl;
 }
